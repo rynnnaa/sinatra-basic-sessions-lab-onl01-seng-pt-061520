8aa5404dbd77a3da770dd61412928ffb765c31db
Ryna Truong <rynatruong@outlook.com>
2020-09-16 21:20:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :sessions_secret, "1a2b3c"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+    @session = session
+    session[:item] = params[:item]
+    
+    erb :checkout
+  end
 end
