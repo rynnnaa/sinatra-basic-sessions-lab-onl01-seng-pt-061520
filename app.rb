6a2e36390bc145b8b329ebf5ccd15b74c7bcddff
Ryna Truong <rynatruong@outlook.com>
2020-09-16 21:19:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,12 +8,13 @@ class App < Sinatra::Base
   end
   
   get '/' do
-    @session = session
     erb :index
   end
   
   post '/checkout' do
-    @session = params[:item]
+    @session = session
+    session[:item] = params[:item]
+    
     erb :checkout
   end
 end
